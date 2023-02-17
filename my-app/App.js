6318f2efc68aa87685a5be666e507fc724cf9239
Matthew Van Brummelen <vb.matthew@gmail.com>
2023-02-16 16:53:05 -0800

    fixing commented out portions

--- my-app/App.js
+++ my-app/App.js
@@ -1,22 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./Screens/HomeScreen";
-import LoginScreen from "./Screens/LoginScreen";
-import RouletteScreen from "./Screens/RouletteScreen";
-import SlotMachineScreen from "./Screens/SlotMachineScreen";
-import SpinTheWheelScreen from "./Screens/SpinTheWheelScreen";
+// import LoginScreen from "./Screens/LoginScreen";
+// import RouletteScreen from "./Screens/RouletteScreen";
+// import SlotMachineScreen from "./Screens/SlotMachineScreen";
+// import SpinTheWheelScreen from "./Screens/SpinTheWheelScreen";
 
 export default function App() {
 	return (
 		<NavigationContainer>
 			<Stack.Navigator>
 				<Stack.Screen name="Home" component={HomeScreen} />
-				<Stack.Screen name="Login" component={LoginScreen} />
+				{/* <Stack.Screen name="Login" component={LoginScreen} />
 				<Stack.Screen name="Roulette" component={RouletteScreen} />
 				<Stack.Screen name="Slots" component={SlotMachineScreen} />
-				<Stack.Screen name="Spin The Wheel" component={SpinTheWheelScreen} />
+				<Stack.Screen name="Spin The Wheel" component={SpinTheWheelScreen} /> */}
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
---
+++ my-app/Screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { useTailwind } from "tailwind-rn";
+
+const tw = useTailwind();
+
+const HomeScreen = () => {
+	return (
+		<View>
+			<Text>Home Screen</Text>
+		</View>
+	);
+};
+
+export default HomeScreen;
