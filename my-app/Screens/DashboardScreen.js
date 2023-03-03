b5aadd833c0275cb9eec33d5a655e78a5a2f6dc4
hsmethurst <harry.smethurst@gmail.com>
2023-03-03 13:04:39 -0800

    fixed dashboard

--- my-app/Screens/DashboardScreen.js
+++ my-app/Screens/DashboardScreen.js
@@ -5,12 +5,30 @@ const DashboardScreen = ({ navigation }) => {
 	return (
 		<View className='flex items-center mt-6'>
 			<Text>Dashboard</Text>
-			<Text
-			className='font-bold text-lg'
-			onPress={() => navigation.navigate("Login")}
-			>
-			Login
-			</Text>
+			<View className='flex items-center mt-6 bg-rose-500 p-3 rounded-full border-2 border-rose-700 w-1/2'>
+				<Text
+				className='font-bold text-lg text-white text-center'
+				onPress={() => navigation.navigate("Roulette")}
+				>
+				Roulette
+				</Text>
+			</View>
+			<View className='flex items-center mt-6 bg-rose-500 p-3 rounded-full border-2 border-rose-700 w-1/2'>
+				<Text
+				className='font-bold text-lg text-white text-center'
+				onPress={() => navigation.navigate("Slots")}
+				>
+				Slots
+				</Text>
+			</View>
+			<View className='flex items-center mt-6 bg-rose-500 p-3 rounded-full border-2 border-rose-700 w-1/2'>
+				<Text
+				className='font-bold text-lg text-white text-center'
+				onPress={() => navigation.navigate("Spin The Wheel")}
+				>
+				Spin The Wheel
+				</Text>
+			</View>
       </View>
 	);
 };
