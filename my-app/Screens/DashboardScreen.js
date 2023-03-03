2defd9b20b348dd28f1d3df44e2cd32c7c4d08fa
hsmethurst <harry.smethurst@gmail.com>
2023-03-03 13:46:43 -0800

    added image

--- my-app/Screens/DashboardScreen.js
+++ my-app/Screens/DashboardScreen.js
@@ -3,33 +3,35 @@ import { View, Text, StyleSheet, Image } from "react-native";
 
 const DashboardScreen = ({ navigation }) => {
 	return (
-		<View className='flex items-center mt-6'>
-			<Text>Dashboard</Text>
-			<View className='flex items-center mt-6 bg-rose-500 p-3 rounded-full border-2 border-rose-700 w-1/2'>
-				<Text
-				className='font-bold text-lg text-white text-center'
-				onPress={() => navigation.navigate("Roulette")}
-				>
-				Roulette
-				</Text>
-			</View>
-			<View className='flex items-center mt-6 bg-rose-500 p-3 rounded-full border-2 border-rose-700 w-1/2'>
-				<Text
-				className='font-bold text-lg text-white text-center'
-				onPress={() => navigation.navigate("Slots")}
-				>
-				Slots
-				</Text>
-			</View>
-			<View className='flex items-center mt-6 bg-rose-500 p-3 rounded-full border-2 border-rose-700 w-1/2'>
-				<Text
-				className='font-bold text-lg text-white text-center'
-				onPress={() => navigation.navigate("Spin The Wheel")}
-				>
-				Spin The Wheel
-				</Text>
-			</View>
-      </View>
+		<View className="flex-1 bg-money bg-repeat bg-center bg-cover">
+			<View className='flex items-center mt-6'>				
+				<Text>Dashboard</Text>
+				<View className='flex items-center mt-6 bg-rose-500 p-3 rounded-full border-2 border-rose-700 w-1/2'>
+					<Text
+					className='font-bold text-lg text-white text-center'
+					onPress={() => navigation.navigate("Roulette")}
+					>
+					Roulette
+					</Text>
+				</View>
+				<View className='flex items-center mt-6 bg-rose-500 p-3 rounded-full border-2 border-rose-700 w-1/2'>
+					<Text
+					className='font-bold text-lg text-white text-center'
+					onPress={() => navigation.navigate("Slots")}
+					>
+					Slots
+					</Text>
+				</View>
+				<View className='flex items-center mt-6 bg-rose-500 p-3 rounded-full border-2 border-rose-700 w-1/2'>
+					<Text
+					className='font-bold text-lg text-white text-center'
+					onPress={() => navigation.navigate("Spin The Wheel")}
+					>
+					Spin The Wheel
+					</Text>
+				</View>
+		</View>
+	  </View>
 	);
 };
 
