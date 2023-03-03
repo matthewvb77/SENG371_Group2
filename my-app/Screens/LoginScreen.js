3b29cdb8b0b6cd2bbea7250eff20cbe499f25799
Levi <47727379+lbare@users.noreply.github.com>
2023-03-02 16:57:52 -0800

    change to nativewind styling

--- my-app/Screens/LoginScreen.js
+++ my-app/Screens/LoginScreen.js
@@ -1,56 +1,38 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { View, Text, TextInput, Button, TouchableOpacity } from 'react-native';
 
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   return (
-    <View>
-      <View>
-        <Text style={styles.title}>Login</Text>
+    <View className='flex-1'>
+      <View className='flex items-center'>
+        <Text className='text-4xl font-bold mt-12'>Login</Text>
       </View>
-      <View>
+      <View className='mt-12 px-5'>
         <TextInput
-          style={styles.input}
+          className='h-12 px-4 bg-gray-100 border border-gray-500 rounded-lg mb-4'
           placeholder='Email'
           onChangeText={(value) => setEmail(value)}
         />
         <TextInput
-          style={styles.input}
+          className='h-12 px-4 bg-gray-100 border border-gray-500 rounded-lg mb-4'
           placeholder='Password'
           onChangeText={(value) => setPassword(value)}
         />
+        <TouchableOpacity className='items-center bg-blue-500 text-white py-3 px-6 rounded-lg'>
+          <Text className='text-lg text-white'>Login</Text>
+        </TouchableOpacity>
       </View>
-      <View>
-        <Text style={styles.text}>Forgot Password?</Text>
-        <Text style={styles.text}>
-          Don't have an account?{' '}
-          <Text style={{ fontWeight: 'bold' }}>Sign Up</Text>
+      <View className='flex items-center mt-6'>
+        <Text className='text-lg'>Forgot Password?</Text>
+        <Text className='text-lg'>
+          Don't have an account? <Text className='font-bold'>Sign Up</Text>
         </Text>
       </View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  title: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginTop: 50,
-  },
-  text: {
-    fontSize: 20,
-    textAlign: 'center',
-    marginTop: 20,
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
-});
 
 export default LoginScreen;
