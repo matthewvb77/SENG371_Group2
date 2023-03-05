8418722e6d42268779e7bcd7ff004e4ac0e03fab
lbare <levi.bare@gmail.com>
2023-03-05 11:27:00 -0800

    configure Roulette screen

--- my-app/Screens/RouletteScreen.js
+++ my-app/Screens/RouletteScreen.js
@@ -1,45 +1,50 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import Button from "../components/Button";
+import { Diamond } from "phosphor-react-native";
 
 const RouletteScreen = () => {
-  const [bet, setBet] = useState("");
-  const [result, setResult] = useState("");
+  const [bet, setBet] = useState(0);
+  const [result, setResult] = useState(null);
 
   return (
-    <View className='flex items-center my-6'>
-      <View className='h-2/3'>
-        <Text>{result}</Text>
+    <View className='flex flex-col h-full w-full justify-center items-center'>
+      <View className='m-10'>
+        <Text className='text-bold font-black text-5xl'>
+          {result || "PLACE A BET!"}
+        </Text>
       </View>
-      <View className='flex flex-row space-x-5 mt-6 mx-5'>
-        <Button
-          ButtonStyle='bg-green-700 border-green-900 w-1/2 mx-2'
-          title='1 - 18'
-        />
-        <Button
-          ButtonStyle='bg-green-700 border-green-900 w-1/2 mx-2'
-          title='19 - 36'
-        />
-      </View>
-      <View className='flex flex-row space-x-5 mt-6 mx-5'>
-        <Button
-          ButtonStyle='bg-green-700 border-green-900 w-1/2 mx-2'
-          title='Even'
-        />
-        <Button
-          ButtonStyle='bg-green-700 border-green-900 w-1/2 mx-2'
-          title='Odd'
-        />
-      </View>
-      <View className='flex flex-row space-x-5 mt-6 mx-5'>
-        <Button
-          ButtonStyle='bg-green-700 border-green-900 w-1/2 mx-2'
-          title='Black'
-        />
-        <Button
-          ButtonStyle='bg-green-700 border-green-900 w-1/2 mx-2'
-          title='Red'
-        />
+      <View className='flex justify-center content-center h-1/3 mb-7'>
+        <View className='flex flex-row mt-6 mx-5 space-5'>
+          <Button
+            ButtonStyle='bg-green-700 border-green-900 w-1/2 rounded-r-none'
+            title='1 - 18'
+          />
+          <Button
+            ButtonStyle='bg-green-700 border-green-900 w-1/2 rounded-l-none'
+            title='19 - 36'
+          />
+        </View>
+        <View className='flex flex-row space-x-5 mt-6 mx-5'>
+          <Button
+            ButtonStyle='bg-green-700 border-green-900 w-1/2 rounded-r-none'
+            title='Even'
+          />
+          <Button
+            ButtonStyle='bg-green-700 border-green-900 w-1/2 rounded-l-none'
+            title='Odd'
+          />
+        </View>
+        <View className='flex flex-row space-x-5 mt-6 mx-5'>
+          <Button
+            ButtonStyle='bg-green-700 border-green-900 w-1/2 rounded-r-none'
+            icon={<Diamond size={40} color='#1F2937' weight='fill' />}
+          />
+          <Button
+            ButtonStyle='bg-green-700 border-green-900 w-1/2 rounded-l-none'
+            icon={<Diamond size={40} color='#F43F3F' weight='fill' />}
+          />
+        </View>
       </View>
     </View>
   );
