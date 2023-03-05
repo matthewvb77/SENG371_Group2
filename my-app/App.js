718a3a8ca2ca3e20910a6d912b52132c378b6c03
lbare <levi.bare@gmail.com>
2023-03-05 10:53:15 -0800

    start Roulette screen

--- my-app/App.js
+++ my-app/App.js
@@ -10,24 +10,33 @@ import SpinTheWheelScreen from "./Screens/SpinTheWheelScreen";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen name="Dashboard" component={DashboardScreen} />
-				<Stack.Screen name="Login" component={LoginScreen} />
-				<Stack.Screen name="Roulette" component={RouletteScreen} />
-				<Stack.Screen name="Slots" component={SlotMachineScreen} />
-				<Stack.Screen name="Spin The Wheel" component={SpinTheWheelScreen} />
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Dashboard' component={DashboardScreen} />
+        <Stack.Screen name='Login' component={LoginScreen} />
+        <Stack.Screen
+          name='Roulette'
+          component={RouletteScreen}
+          options={{
+            headerShown: true,
+            animationEnabled: false,
+            headerBackTitleVisible: false,
+            headerLeft: null,
+          }}
+        />
+        <Stack.Screen name='Slots' component={SlotMachineScreen} />
+        <Stack.Screen name='Spin The Wheel' component={SpinTheWheelScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
--- my-app/Screens/RouletteScreen.js
+++ my-app/Screens/RouletteScreen.js
@@ -1,12 +1,48 @@
-import React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import Button from "../components/Button";
 
 const RouletteScreen = () => {
-	return (
-		<View>
-			<Text>Roulette</Text>
-		</View>
-	);
+  const [bet, setBet] = useState("");
+  const [result, setResult] = useState("");
+
+  return (
+    <View className='flex items-center my-6'>
+      <View className='h-2/3'>
+        <Text>{result}</Text>
+      </View>
+      <View className='flex flex-row space-x-5 mt-6 mx-5'>
+        <Button
+          ButtonStyle='bg-green-700 border-green-900 w-1/2 mx-2'
+          title='1 - 18'
+        />
+        <Button
+          ButtonStyle='bg-green-700 border-green-900 w-1/2 mx-2'
+          title='19 - 36'
+        />
+      </View>
+      <View className='flex flex-row space-x-5 mt-6 mx-5'>
+        <Button
+          ButtonStyle='bg-green-700 border-green-900 w-1/2 mx-2'
+          title='Even'
+        />
+        <Button
+          ButtonStyle='bg-green-700 border-green-900 w-1/2 mx-2'
+          title='Odd'
+        />
+      </View>
+      <View className='flex flex-row space-x-5 mt-6 mx-5'>
+        <Button
+          ButtonStyle='bg-green-700 border-green-900 w-1/2 mx-2'
+          title='Black'
+        />
+        <Button
+          ButtonStyle='bg-green-700 border-green-900 w-1/2 mx-2'
+          title='Red'
+        />
+      </View>
+    </View>
+  );
 };
 
 export default RouletteScreen;
