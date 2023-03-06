cff4d6116b240baca219637a0cd17f6de824a575
Levi <47727379+lbare@users.noreply.github.com>
2023-03-05 21:53:16 -0800

    fix merge conflict

--- my-app/components/Button.js
+++ my-app/components/Button.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Text, TouchableOpacity } from "react-native";
 
-const Button = ({ title, onPress, ButtonStyle, TextStyle }) => {
+const Button = ({ title, onPress, ButtonStyle, TextStyle, icon }) => {
   return (
     <TouchableOpacity
       className={`flex items-center p-3 rounded-lg border-2 ${ButtonStyle}`}
       onPress={onPress}
     >
-      <Text className={`font-bold text-lg text-white text-center ${TextStyle}`}>
-        {title}
-      </Text>
+      {icon || (
+        <Text
+          className={`font-bold text-lg text-white text-center ${TextStyle}`}
+        >
+          {title}
+        </Text>
+      )}
     </TouchableOpacity>
   );
 };
