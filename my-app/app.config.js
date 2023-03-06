98af1282fd3c44334eb9b60e032058e9887c7c63
Matthew Van Brummelen <mvanbrummelen@uvic.ca>
2023-03-05 22:45:22 -0800

    fixed some mistakes that happened when resolving merge conflicts

--- my-app/app.config.js
+++ my-app/app.config.js
@@ -1,55 +1,46 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 export default {
-import 'dotenv/config';
-
-export default {
-  "expo": {
-    "name": "my-app",
-    "slug": "my-app",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/icon.png",
-    "userInterfaceStyle": "light",
-    "splash": {
-      "image": "./assets/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
-    },
-    "updates": {
-      "fallbackToCacheTimeout": 0
-    },
-    "assetBundlePatterns": [
-      "**/*"
-    ],
-    "ios": {
-      "supportsTablet": true,
-      "bundleIdentifier": "com.group2.casino",
-      "googleServicesFile": "./GoogleService-Info.plist"
-      "bundleIdentifier": "com.group2.casino",
-      "googleServicesFile": "./GoogleService-Info.plist"
-    },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#FFFFFF"
-      },
-      "package": "com.group2.casino",
-      "googleServicesFile": "./google-services.json"
-      "package": "com.group2.casino",
-      "googleServicesFile": "./google-services.json"
-    },
-    "web": {
-      "favicon": "./assets/favicon.png"
-    },
-    "extra": {
-      firebaseApiKey: process.env.FIREBASE_API_KEY,
-      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
-      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      firebaseAppId: process.env.FIREBASE_APP_ID
-    },
-    "scheme": "my-app"
-  }
-}
+	expo: {
+		name: "my-app",
+		slug: "my-app",
+		version: "1.0.0",
+		orientation: "portrait",
+		icon: "./assets/icon.png",
+		userInterfaceStyle: "light",
+		splash: {
+			image: "./assets/splash.png",
+			resizeMode: "contain",
+			backgroundColor: "#ffffff",
+		},
+		updates: {
+			fallbackToCacheTimeout: 0,
+		},
+		assetBundlePatterns: ["**/*"],
+		ios: {
+			supportsTablet: true,
+			bundleIdentifier: "com.group2.casino",
+			googleServicesFile: "./GoogleService-Info.plist",
+		},
+		android: {
+			adaptiveIcon: {
+				foregroundImage: "./assets/adaptive-icon.png",
+				backgroundColor: "#FFFFFF",
+			},
+			package: "com.group2.casino",
+			googleServicesFile: "./google-services.json",
+		},
+		web: {
+			favicon: "./assets/favicon.png",
+		},
+		extra: {
+			firebaseApiKey: process.env.FIREBASE_API_KEY,
+			firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+			firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+			firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+			firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+			firebaseAppId: process.env.FIREBASE_APP_ID,
+		},
+		scheme: "my-app",
+	},
+};
