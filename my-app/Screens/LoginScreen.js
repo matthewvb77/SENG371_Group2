f55442a5dd40b988beb4bce7825839286a4a1026
Matthew Van Brummelen <mvanbrummelen@uvic.ca>
2023-03-05 23:50:24 -0800

    fixed merge conflicts

--- my-app/Screens/LoginScreen.js
+++ my-app/Screens/LoginScreen.js
@@ -26,7 +26,6 @@ const LoginScreen = ({ navigation }) => {
 			"162824721551-9u964a98m0uk5pl7jjf8b3nfjg3ut4ut.apps.googleusercontent.com",
 	});
 
-<<<<<<< HEAD
 	useEffect(() => {
 		if (response?.type === "success") {
 			setToken(response.authentication.accessToken);
@@ -168,63 +167,6 @@ const LoginScreen = ({ navigation }) => {
 			</View>
 		</View>
 	);
-=======
-  const handleGoogleLoginPress = () => {
-    // TODO
-  };
-
-  return (
-    <View className='flex-1 pt-10'>
-      <View className='flex items-center'>
-        <Text className='text-4xl font-bold mt-12'>Login</Text>
-      </View>
-      <View className='mt-12 px-5'>
-        <TextInput
-          className='h-12 px-4 bg-gray-100 border border-gray-500 rounded-lg mb-4'
-          placeholder='Email'
-          onChangeText={(value) => setEmail(value)}
-        />
-        <TextInput
-          className='h-12 px-4 bg-gray-100 border border-gray-500 rounded-lg mb-4'
-          placeholder='Password'
-          onChangeText={(value) => setPassword(value)}
-        />
-        <TouchableOpacity
-          onPress={handleLoginPress}
-          className='items-center bg-blue-500 text-white py-3 px-6 rounded-lg mb-4'
-        >
-          <Text className='text-lg text-white'>Login</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          onPress={handleGoogleLoginPress}
-          className='items-center justify-center flex flex-row space-x-5 bg-gray-700 py-3 px-6 rounded-lg'
-        >
-          <Image
-            className='h-6 w-6'
-            source={{
-              uri: "https://i.ibb.co/j82DCcR/search.png",
-            }}
-          />
-          <Text className='text-lg text-white'>Sign in with Google</Text>
-        </TouchableOpacity>
-      </View>
-      <View className='flex items-center mt-6'>
-        <Text className='text-lg'>Forgot Password?</Text>
-        <Text className='text-lg'>
-          Don't have an account?
-          <Text
-            className='font-bold'
-            onPress={() => navigation.navigate("Register")}
-          >
-            {" "}
-            Sign Up
-          </Text>
-        </Text>
-      </View>
-    </View>
-  );
->>>>>>> e62ec46 (style google sign in button)
 };
 
 const styles = StyleSheet.create({
