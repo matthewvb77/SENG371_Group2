25fcc5282eeaaabed4082577baa78b9ae7ded8cf
Levi <47727379+lbare@users.noreply.github.com>
2023-03-12 15:41:36 -0700

    add firebase credentials back

--- my-app/firebase.js
+++ my-app/firebase.js
@@ -1,28 +1,28 @@
-import { getApps, initializeApp, getApp } from "firebase/app";
-import Constants from "expo-constants";
+import { getApps, initializeApp, getApp } from 'firebase/app';
+import Constants from 'expo-constants';
 import {
-	getAuth,
-	createUserWithEmailAndPassword,
-	signInWithEmailAndPassword,
-	GoogleAuthProvider,
-} from "firebase/auth";
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+} from 'firebase/auth';
 import {
-	getFirestore,
-	collection,
-	addDoc,
-	docRef,
-	doc,
-	getDoc,
-	setDoc,
-} from "firebase/firestore";
+  getFirestore,
+  collection,
+  addDoc,
+  docRef,
+  doc,
+  getDoc,
+  setDoc,
+} from 'firebase/firestore';
 
 const firebaseConfig = {
-	apiKey: Constants.manifest.extra.firebaseApiKey,
-	authDomain: Constants.manifest.extra.firebaseAuthDomain,
-	projectId: Constants.manifest.extra.firebaseProjectId,
-	storageBucket: Constants.manifest.extra.firebaseStorageBucket,
-	messagingSenderId: Constants.manifest.extra.firebaseMessagingSenderId,
-	appId: Constants.manifest.extra.firebaseAppId,
+  apiKey: 'AIzaSyBISxRAkqhe-I16pHdXjogT_sWwSAyz4f4',
+  authDomain: 'casino-90ba4.firebaseapp.com',
+  projectId: 'casino-90ba4',
+  storageBucket: 'casino-90ba4.appspot.com',
+  messagingSenderId: '162824721551',
+  appId: '1:162824721551:web:03914cea5c3ae15266ccfa',
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
@@ -30,14 +30,14 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 export {
-	db,
-	auth,
-	collection,
-	addDoc,
-	createUserWithEmailAndPassword,
-	signInWithEmailAndPassword,
-	docRef,
-	doc,
-	getDoc,
-	setDoc,
+  db,
+  auth,
+  collection,
+  addDoc,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  docRef,
+  doc,
+  getDoc,
+  setDoc,
 };
