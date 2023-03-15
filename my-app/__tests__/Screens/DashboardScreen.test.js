9d709c42480da3452f0e44055914108b42147733
lbare <levi.bare@gmail.com>
2023-03-14 18:34:13 -0700

    update test snapshots

--- my-app/__tests__/Screens/DashboardScreen.test.js
+++ my-app/__tests__/Screens/DashboardScreen.test.js
@@ -1,11 +1,10 @@
 import React from "react";
 import renderer from "react-test-renderer";
-
 import DashboardScreen from "../../Screens/DashboardScreen";
 
 describe("<DashboardScreen />", () => {
-	it("renders correctly", () => {
-		const tree = renderer.create(<DashboardScreen />).toJSON();
-		expect(tree).toMatchSnapshot();
-	});
+  it("renders correctly", () => {
+    const tree = renderer.create(<DashboardScreen />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
